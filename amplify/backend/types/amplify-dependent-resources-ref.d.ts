04eb57ba8845e11bcf86a6d00cb4257079b46e51
aakashbshendage <aakashshendage3902@gmail.com>
2023-11-04 22:48:08 +0530

    Vue2AWSAuthAmp Working

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "vueamplifyauthtutori8141ed19": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string",
+            "AppClientSecret": "string"
+        }
+    },
+    "api": {
+        "vueamplifyauthtutori": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
